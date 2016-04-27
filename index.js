2998eacd303a730c05f89148fa68cd56f747fda6
mfjv88 <mfjvacas@gmail.com>
2016-04-27 18:45:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port!' + port);
 });
